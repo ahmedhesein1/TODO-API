01688a5a15ba1bb8de2ee2cabc562770c30a97c1
ahmedhesein1 <ahmedhesein1790@gmail.com>
2025-03-14 16:43:38 +0200

    First commit

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,14 @@ import express, { Express } from 'express';
 import dotenv from 'dotenv';
 import { DataSource } from 'typeorm';
 import mysql from 'mysql2/promise';
-
+import bodyParser from 'body-parser';
+import cors from 'cors';
 dotenv.config();
 const app: Express = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(bodyParser.json());
+app.use(cors());
 // ✅ Define AppDataSource at the top level (don't put `export` inside a function)
 export const AppDataSource = new DataSource({
   type: 'mysql',
