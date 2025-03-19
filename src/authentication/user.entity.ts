ef66bf7779a8285d70f83795cb7c0f5744ae80bf
ahmedhesein1 <ahmedhesin1790@gmail.com>
2025-03-19 22:28:55 +0200

    Implementing user schema

---
+++ src/authentication/user.entity.ts
@@ -0,0 +1,41 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { Role } from '../enums/role';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+  @Column({
+    type: 'varchar',
+    length: 225,
+  })
+  name: string;
+  @Column({
+    type: 'varchar',
+    length: 225,
+    unique: true,
+  })
+  email: string;
+  @Column({
+    type: 'varchar',
+    length: 225,
+    select: false,
+  })
+  password: string;
+  @Column({
+    type: 'enum',
+    enum: Role,
+    default: Role.user,
+  })
+  role: Role;
+  @CreateDateColumn()
+  createdAt: Date;
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
